9830c0ff325abb219f816bfa7b458cfe5ca794bf
Rahul Waghmare <rahulwaghmare1711@gmail.com>
2024-07-28 16:04:56 +0530

    problems solved

--- assignment1/problem_1.js
+++ assignment1/problem_1.js
@@ -1,7 +1,19 @@
-console.log("Hello World!" + "\n" + "Hello World! How are you doing!!");
-/*
-The output is given below
+// console.log("Hello World!" + "\n" + "Hello World! How are you doing!!");
+// /*
+// The output is given below
 
-Hello World!
-Hello World! How are you doing!!
-*/
+// Hello World!
+// Hello World! How are you doing!!
+// */
+// console.log(23%3)
+
+let x = 60;
+let y = 36;
+
+if(y==0){
+    console.log(x)
+}else{
+    x=y;
+    y = x%y;
+}
+console.log(x,y)
