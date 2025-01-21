c5644aac20bb18c7fa5829735b49bd1067847df4
Rahul Waghmare <157009841+rahul-17-11@users.noreply.github.com>
2025-01-21 10:33:09 +0530

    Delete pbs_4 directory

--- pbs_4/solution1.js
+++
@@ -1,9 +0,0 @@
-triangleArea = function(base,height){
-    let x = (base*height)*(1/2)
-    if(x!==0 && base>0 && height>0){
-        console.log(`The area of Triangle is: ${x}`)
-    }else{
-        console.log("Invalid number, base and height must be positive number ")
-    }
-}
-triangleArea(10,0.5)
