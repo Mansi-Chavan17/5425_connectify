cac1b1fb9ec4c4d84faa7f6acfd85f22de7ca22f
Rahul Waghmare <157009841+rahul-17-11@users.noreply.github.com>
2025-01-21 10:32:34 +0530

    Delete jsfunctions1 directory

--- jsfunctions1/findPower.js
+++
@@ -1,8 +0,0 @@
-function calculatePower(base,exponent){
-    let power = base**exponent
-    console.log(power)
-}
-
-let a = 5
-let b = 3
-calculatePower(a,b)
--- jsfunctions1/findSum.js
+++
@@ -1,8 +0,0 @@
-function calculateSum(a,b){
-    let sum = a+b
-    console.log(sum)
-}
-
-let a = 5
-let b = 3
-calculateSum(a,b)
